268a8190f3f07edccf97775ad1f7ff6b73e4fa1f
sh <sommerh143@yahoo.com>
2020-04-29 17:19:33 -0700

    create questions, and make a submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   $("#start").click(function(){
-    $(".question").show();
+    $(".question").slideDown();
   });
-
+  var age = $("#age").val();
+  var color = $("#color").val();
+  var animal = $("#animal").val();
 });
