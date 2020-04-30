62e3c2b279e1a3cc0f3490721a9d0c3afe06b0f5
sh <sommerh143@yahoo.com>
2020-04-29 20:35:48 -0700

    make a refesh button

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,35 @@ $(document).ready(function(){
   $("#start").click(function(){
     $(".question").slideDown();
   });
+
  var age, color, animal, answer;
 
   $("#finish").click(function(){
-  
-  age = parseInt($("#age").val());
-  color = parseInt($("#color").val());
-  animal = parseInt($("#animal").val());
-  answer = parseInt(age + color + animal);
+    age = parseInt($("#age").val());
+    color = parseInt($("#color").val());
+    animal = parseInt($("#animal").val());
+    game = parseInt($("#game").val());
+    year = parseInt($("#year").val());
+    answer = parseInt(age + color + animal + game + year);
 
-  alert(answer);
-  if (answer == 3){
-  alert("c#");
-  } else if (answer <= 5){
-    alert("jquary");
-    } else if (answer = 6){
-      alert("c");
-      }
+    if (answer == 5){
+      $(".questionAnswer").slideDown();
+      $("span").append("C#");
+    } else if (answer <= 5){
+      $(".questionAnswer").slideDown();
+      $("span").append("Ruby");
+    } else if (answer == 6){
+        $(".questionAnswer").slideDown();
+        $("span").append("Python");
+    }
+    
+    $("#finish").click(function(){
+      location.reload(true);
+    });
 
-  
-})
+    $("#yes").click(function(){
+      location.reload(true);
+    });
+ 
+  });
 });
