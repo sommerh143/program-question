651f01cb6df53756b34a817f48878957bd58cf8c
sh <sommerh143@yahoo.com>
2020-04-29 20:10:48 -0700

    add 2 more questions, start to store the answers in a variable.

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,24 @@ $(document).ready(function(){
   $("#start").click(function(){
     $(".question").slideDown();
   });
- var age, color, animal;
+ var age, color, animal, answer;
 
   $("#finish").click(function(){
   
-  age = $("#age").val();
-  color = $("#color").val();
-  animal = $("#animal").val();
-  alert(age);
+  age = parseInt($("#age").val());
+  color = parseInt($("#color").val());
+  animal = parseInt($("#animal").val());
+  answer = parseInt(age + color + animal);
+
+  alert(answer);
+  if (answer == 3){
+  alert("c#");
+  } else if (answer <= 5){
+    alert("jquary");
+    } else if (answer = 6){
+      alert("c");
+      }
+
   
 })
 });
